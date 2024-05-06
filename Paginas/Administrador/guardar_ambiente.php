8b5f1a3a87807fdc24bd2ca48337f2b0bd8ab254
JHANINASUAREZ <129642373+JHANINASUAREZ@users.noreply.github.com>
2024-05-06 07:54:39 -0400

    Update guardar_ambiente.php

--- Paginas/Administrador/guardar_ambiente.php
+++ Paginas/Administrador/guardar_ambiente.php
@@ -2,10 +2,10 @@
 require_once '../../config/validacion_session.php';
 require_once '../../config/conexion.php';
 // Conexión a la base de datos (reemplaza los valores con los de tu servidor)
-$servername = "monorail.proxy.rlwy.net";
+$servername = "localhost";
 $username = "root";
-$password = "uiszwdaBOhGxlHiwFJHRlbdJbaMLDnqy";
-$dbname = "railway";
+$password = "";
+$dbname = "reservasumss1";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,32 +20,35 @@ $nombre = $_POST['nombre'];
 $capacidad = $_POST['capacidad'];
 $ubicacion = $_POST['ubicacion'];
 $piso = $_POST['piso'];
-$periodo = $_POST['periodo'];
-$fechaInicio = $_POST['fechaInicio'];
-$fechaFin = $_POST['fechaFin'];
-$horarios = implode(",", $_POST['horarios']); // Concatenar los horarios seleccionados en una cadena separada por comas
 
+$fecha = $_POST['fecha'];
+$descripcion = $_POST['descripcion'];
 
 
 $imgAmbiente = $_FILES["imgAmbiente"];
 $nameImagen = $imgAmbiente["name"];
 $tmpImagen = $imgAmbiente["tmp_name"];
 
+
 move_uploaded_file($tmpImagen, "../../Img/Ambientes/" . $nameImagen);
+$sql = "INSERT INTO ambientes (nombre) VALUES ('Ambiente de prueba')";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Registro insertado correctamente";
+} else {
 // Insertar los datos en la base de datos
-$sql = "INSERT INTO ambientes (nombre, capacidad, ubicacion, piso, periodo, fechainicio, fechafin, horarios, imgAmbiente)
-        VALUES ('$nombre', '$capacidad', '$ubicacion', '$piso', '$periodo', '$fechaInicio', '$fechaFin','$horarios', '$nameImagen')";
+$sql = "INSERT INTO ambientes (nombre, capacidad, ubicacion, piso, fecha, descripcion, imgAmbiente)
+        VALUES ('$nombre', '$capacidad', '$ubicacion', '$piso', '$fecha', '$descripcion','$nameImagen')";
 
-if(isset($_FILES["imgAmbiente"]) && $_FILES["imgAmbiente"]["error"] === 0);{
+if (isset($_FILES["imgAmbiente"]) && $_FILES["imgAmbiente"]["error"] === 0); {
 }
 
 if ($conn->query($sql) === TRUE) {
-    echo "Datos insertados correctamente"; // Envía una respuesta al cliente indicando que la operación se completó con éxito
+    header("Location: RegistrodeAmbiente.php");; // Envía una respuesta al cliente indicando que la operación se completó con éxito
 } else {
     echo "Erroral insertar datos: " . $sql . "<br>" . $conn->error;
 }
 
 // Cerrar la conexión
 $conn->close();
-?>
+}
